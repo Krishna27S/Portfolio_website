09027090aae7c2c5029550d567b593440d06760c
Krishna Shekhar <krishnashekhar27@gmail.com>
2024-10-15 02:24:02 +0530

    updates

--- api/contact.js
+++ api/contact.js
@@ -1,14 +1,13 @@
-import nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method === 'POST') {
     const { name, email, message } = req.body;
 
-    // Create a transporter using SMTP
     let transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
-      secure: false, // Use TLS
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -16,7 +15,6 @@ export default async function handler(req, res) {
     });
 
     try {
-      // Send email
       await transporter.sendMail({
         from: process.env.EMAIL_USER,
         to: "krishnashekhar27@gmail.com", // Replace with your email
@@ -36,4 +34,4 @@ export default async function handler(req, res) {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-}
+};
